b4aaf70f7cf183d342701512bedb46bbe5565d8f
razzerone <Kirill.Sopegin@urfu.me>
2022-04-01 16:56:00 +0500

    Добавлен проект для логики взаимодействия

---
+++ ManchkinOriginalLogic/CoreExtensions/ClassExtension.cs
@@ -0,0 +1,6 @@
+namespace ManchkinOriginalLogic.CoreExtensions;
+
+public class ClassExtension
+{
+    // ToDo: Механизм обЪединения классов (суперманчкин)
+}
---
+++ ManchkinOriginalLogic/CoreExtensions/RaceExtension.cs
@@ -0,0 +1,7 @@
+namespace ManchkinOriginalLogic.CoreExtensions;
+
+public static class RaceExtension
+{
+    // public static IRace CombineRace(this IRace originalRace, IRace otherRace)
+    // ToDO: Механизм объединения расс (полукровка)
+}

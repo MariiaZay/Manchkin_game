619eea13d00d901c0b7538d98ef6652da0f48d0b
KoloskovaMariia <koloskovamariia@gmail.com>
2022-05-23 15:51:08 +0500

    description

--- ManchkinGame/DialogWindows/DescriptionWindow.xaml.cs
+++ ManchkinGame/DialogWindows/DescriptionWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace ManchkinGame;
 
@@ -20,15 +22,40 @@ public partial class DescriptionWindow : Window
 
     private void DescriptionScrollViewLoaded(object sender, RoutedEventArgs e)
     {
-        var sp = new StackPanel();
-        foreach (var description in _descriptions)
+        var desc = _descriptions.ToArray();
+        for (var i = 0; i < desc.Length; i++)
         {
-            var textBlock = new TextBlock();
-            textBlock.Text = description;
-            sp.Children.Add(textBlock);
+            var grid = new Grid();
+            
+            if (i == 0)
+                grid.Margin = new Thickness(0, 0, 0, 4.4);
+            else
+            {
+                grid.Margin = i == desc.Length - 1 ? 
+                    new Thickness(0, 4.4, 0, 0) 
+                    : new Thickness(0, 4.4, 0, 4.4);
+            }
+            var colDef0 =new ColumnDefinition();
+            colDef0.Width = new GridLength(0.05, GridUnitType.Star);
+            var colDef1 =new ColumnDefinition();
+            grid.ColumnDefinitions.Add(colDef0);
+            grid.ColumnDefinitions.Add(colDef1);
+            
+            var ellipce = new Ellipse();
+            ellipce.Style = (Style) FindResource("EllipseStyle");
+            Grid.SetColumn(ellipce,0);
+
+            var text = new TextBlock();
+            text.Style = (Style) FindResource("TextStyle");
+            text.Text = desc[i];
+            Grid.SetColumn(text,1);
+
+            grid.Children.Add(ellipce);
+            grid.Children.Add(text);
+            DescriptionStackPanel.Children.Add(grid);
         }
 
-        DescriptionScrollView.Content = sp;
+        DescriptionScrollView.Content = DescriptionStackPanel;
     }
 
     private void OkButtonClick(object sender, RoutedEventArgs e)

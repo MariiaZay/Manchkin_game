91e70943a4090f9f85eb0275acb437e77c68e2f6
razzerone <newrazzerone@gmail.com>
2022-05-24 01:06:53 +0500

    Making method, that give you actions which you can do on your turn (not implemented yet)

---
+++ ManchkinCore/GameStates/GameActions.cs
@@ -0,0 +1,14 @@
+﻿namespace ManchkinCore.GameStates;
+
+public enum GameActions
+{
+    ChangeRace,
+    DropRace,
+    ChangeClass,
+    DropClass,
+    UseStuff,
+    Sell,
+    Change,
+    PlayCard,
+    
+}
---
+++ ManchkinCore/GameStates/GameRules.cs
@@ -0,0 +1,23 @@
+﻿namespace ManchkinCore.GameStates;
+
+public static class GameRules
+{
+    private static readonly HashSet<GameActions> BaseActions = 
+        new() {GameActions.DropRace, GameActions.DropClass};
+    
+    private static readonly HashSet<GameActions> NonBattleActions = 
+        new() {GameActions.ChangeClass, GameActions.ChangeRace,GameActions.Change, GameActions.Sell,};
+
+    public static IReadOnlySet<GameActions> GetTurnActions(bool isPlayerTurn, bool isInBattle)
+    {
+        if (!isPlayerTurn)
+            return new HashSet<GameActions>()
+            {
+
+            };
+        if (isInBattle)
+            return BaseActions;
+        return BaseActions.Union(NonBattleActions).ToHashSet();
+
+    }
+}

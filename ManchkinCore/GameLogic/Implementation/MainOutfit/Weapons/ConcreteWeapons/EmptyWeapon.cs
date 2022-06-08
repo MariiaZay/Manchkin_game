b5c76b09224809da523ddb4838aa190e6a706e48
KoloskovaMariia <koloskovamariia@gmail.com>
2022-06-08 13:01:52 +0500

    df

--- ManchkinCore/GameLogic/Implementation/MainOutfit/Weapons/ConcreteWeapons/EmptyWeapon.cs
+++ ManchkinCore/GameLogic/Implementation/MainOutfit/Weapons/ConcreteWeapons/EmptyWeapon.cs
@@ -1,6 +1,24 @@
-﻿namespace ManchkinCore.Implementation;
+﻿using ManchkinCore.Enums.Accessory;
+using ManchkinCore.Interfaces;
 
-public class EmptyWeapon
+namespace ManchkinCore.Implementation;
+
+public class EmptyWeapon : Weapon
 {
-    
+    public EmptyWeapon()
+    {
+        Price = 400;
+        Damage = 2;
+        Weight = Bulkiness.SMALL;
+        Fullness = Arms.SINGLE;
+        Descriptions = new List<string>();
+        FlushingBonus = 0;
+        TextRepresentation = "Меч коварного ублюдка";
+    }
+
+
+    public override bool CanBeUsed(IRace? race) => true;
+    public override bool CanBeUsed(IClass? _class) => true;
+
+    public override bool CanBeUsed(Genders gender) => true;
 }

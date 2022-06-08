df553e1522e41198fc2a8980905674d6ebec234e
KoloskovaMariia <koloskovamariia@gmail.com>
2022-06-08 13:24:21 +0500

    refactor

--- ManchkinCore/GameLogic/Implementation/MainOutfit/Weapons/ConcreteWeapons/EmptyWeapon.cs
+++ ManchkinCore/GameLogic/Implementation/MainOutfit/Weapons/ConcreteWeapons/EmptyWeapon.cs
@@ -1,6 +1,26 @@
-﻿namespace ManchkinCore.Implementation;
+﻿using ManchkinCore.Enums.Accessory;
+using ManchkinCore.Interfaces;
 
-public class EmptyWeapon
+namespace ManchkinCore.Implementation;
+
+public class EmptyWeapon: Weapon
 {
-    
+    public EmptyWeapon()
+    {
+        TextRepresentation = "";
+    }
+    public override bool CanBeUsed(IRace? race)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool CanBeUsed(IClass? _class)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool CanBeUsed(Genders gender)
+    {
+        throw new NotImplementedException();
+    }
 }
